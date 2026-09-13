37f57e84340749a1ed235fccfdfaf761d7c5b301
grae88888888 <grae.cumming@fortescue.com>
2026-09-13 22:39:00 +1000

    model changes

--- .local/k3d/destroy-local.sh
+++ .local/k3d/destroy-local.sh
@@ -2,13 +2,20 @@
 set -e
 
 CLUSTER_NAME="roommate-cluster-local"
+POSTGRES_CONTAINER="roommate-postgres-local"
 
-if ! k3d cluster list | grep -q "$CLUSTER_NAME"; then
+if k3d cluster list | grep -q "$CLUSTER_NAME"; then
+    echo "deleting cluster..."
+    k3d cluster delete "$CLUSTER_NAME"
+else
     echo "cluster '$CLUSTER_NAME' doesn't exist, nothing to do"
-    exit 0
 fi
 
-echo "deleting cluster..."
-k3d cluster delete "$CLUSTER_NAME"
+if docker ps -a --format '{{.Names}}' | grep -q "^${POSTGRES_CONTAINER}$"; then
+    echo "removing postgres container '$POSTGRES_CONTAINER'..."
+    docker rm -f "$POSTGRES_CONTAINER"
+else
+    echo "postgres container '$POSTGRES_CONTAINER' doesn't exist, nothing to do"
+fi
 
 echo "done"
